1f72029ddcb7a2fae8e1e0a464121847045f5685
diznq <13458283+diznq@users.noreply.github.com>
2023-02-14 08:59:33 +0100

    fix: static linking

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
-STATIC=""
 LUA_LIB="/usr/local/lib/liblua.a"
 LUA_INC="/usr/local/include/"
 
@@ -8,4 +7,4 @@ if [[ "${JIT}" == "true" ]]; then
     LUA_INC="/usr/local/include/luajit-2.0/"
 fi
 
-gcc src/main.c src/lua.c "-I$LUA_INC" $STATIC "$LUA_LIB" -lm -s -Ofast -march=native -Wno-pointer-to-int-cast -Wno-int-to-pointer-cast -o bin/server
+gcc src/main.c src/lua.c "-I$LUA_INC" "$LUA_LIB" -lm -s -Ofast -march=native -Wno-pointer-to-int-cast -Wno-int-to-pointer-cast -o bin/server
