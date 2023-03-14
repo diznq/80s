603e92879c22a5456281007401833e9f8b60c310
diznq <13458283+diznq@users.noreply.github.com>
2023-03-14 23:27:58 +0100

    fix: keep SQL connection on server reload

--- server/www/main.lua
+++ server/www/main.lua
@@ -1,7 +1,16 @@
 local mysql = require("server.mysql")
 local orm = require("server.orm")
 
-SQL = mysql:new()
+if not SQL then
+    SQL = mysql:new()
+    local user, password, db = os.getenv("DB_USER") or "80s", os.getenv("DB_PASSWORD") or "password", os.getenv("DB_NAME") or "db80"
+    SQL:connect(user, password, db)(function (ok, err)
+        if not ok then
+            print("Failed to connect to SQL: ", err)
+        end
+    end)
+end
+
 --- @type ormrepo
 Posts = orm:create(SQL, {
     source = "posts",
@@ -14,10 +23,3 @@ Posts = orm:create(SQL, {
     findById = true,
     findBy = true
 })
-
-local user, password, db = os.getenv("DB_USER") or "80s", os.getenv("DB_PASSWORD") or "password", os.getenv("DB_NAME") or "db80"
-SQL:connect(user, password, db)(function (ok, err)
-    if not ok then
-        print("Failed to connect to SQL: ", err)
-    end
-end)
