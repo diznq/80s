f8958cfc809fd964a8a561e8bd1f6e8170b932f8
diznq <13458283+diznq@users.noreply.github.com>
2023-04-08 19:02:31 +0200

    fix: redirect output

--- server/http.lua
+++ server/http.lua
@@ -30,6 +30,11 @@ local function create_endpoint(base, method, endpoint, mime, content, dynamic)
             local session = {}
             local parsed_query = aio:parse_query(query, aio.master_key and endpoint or nil)
             templates:render(self, session, headers, body, method, endpoint, parsed_query, mime, ctx)(function (result)
+                local code = result.status:sub(1, 3)
+                -- in case of redirects, prevent any kind of output
+                if code == "301" or code == "302" then
+                    result.content = ""
+                end
                 self:http_response(result.status, result.headers, result.content)
             end)
         else
