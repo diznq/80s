ed96e69f3c9e29c75067fa70cf895449317d2154
diznq <13458283+diznq@users.noreply.github.com>
2023-04-08 10:41:53 +0200

    fix: lua encoder

--- src/lua_codec.c
+++ src/lua_codec.c
@@ -19,16 +19,26 @@ static void encode_string(struct dynstr *out, const char *value, size_t value_le
         case '\n':
             dynstr_puts(out, "\\n", 2);
             break;
+        case '\t':
+            dynstr_puts(out, "\\t", 2);
+            break;
         case '"':
             dynstr_puts(out, "\\\"", 2);
             break;
         case '\\':
             dynstr_puts(out, "\\\\", 2);
+            break;
         case '\0':
             dynstr_puts(out, "\\0", 2);
             break;
         default:
-            dynstr_putc(out, *value);
+            if(*value < 32) {
+                dynstr_puts(out, "\\x", 2);
+                dynstr_putc(out, "0123456789ABCDEF"[(*value >> 4) & 15]);
+                dynstr_putc(out, "0123456789ABCDEF"[(*value) & 15]);
+            } else {
+                dynstr_putc(out, *value);
+            }
             break;
         }
         value++;
