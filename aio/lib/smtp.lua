99335551e6389e9614d80a89de69a5b35dbc1a4b
diznq <13458283+diznq@users.noreply.github.com>
2023-10-29 21:26:32 +0100

    chore: move smtp handling logic into separate method

--- aio/lib/smtp.lua
+++ aio/lib/smtp.lua
@@ -6,6 +6,7 @@ require("aio.aio")
 
 smtp = {
     counter = 0,
+    host = "localhost",
     --- @type {[string]: mailreceived}
     on_mail_received_callbacks = {}
 }
@@ -13,7 +14,7 @@ smtp = {
 --- Initialize SMTP server
 ---@param params {host: string}
 function smtp:init(params)
-    local smtp_self = self
+    self.host = params.host
     aio:add_protocol_handler("smtp", {
         matches = function (data)
             return true
@@ -22,112 +23,116 @@ function smtp:init(params)
             fd:write("220 " .. params.host .. " ESMTP 80s\r\n")
         end,
         handle = function (fd)
-            aio:buffered_cor(fd, function (resolve)
-                local ok = true
-                local hello = false
-                local from, message, to = nil, nil, {}
-                while ok do
-                    local line = coroutine.yield("\r\n")
-                    if line == nil then return end
-                    local msg_type = line:sub(1, 4)
-                    local handled = false
-                    if msg_type == "HELO" then
-                        hello = true
-                        fd:write("250 HELO " .. line:sub(6) .. "\r\n")
-                        handled = true
-                    elseif msg_type == "EHLO" then
-                        hello = true
-                        fd:write("250-".. params.host .. " is my domain name. Hello " .. line:sub(6) .. "!\r\n250-8BITMIME\r\n250-ENHANCEDSTATUSCODES\r\n250 SIZE 1000000\r\n")
-                        handled = true
-                    elseif msg_type == "MAIL" then
-                        if line:sub(6, 10) == "FROM:" then
-                            local raw_from = line:sub(11)
-                            local parsed_from = self:extract_email(raw_from)
-                            if from ~= nil then
-                                fd:write("555 MAIL FROM was already sent previously\r\n")
-                                handled = true
-                            elseif parsed_from then
-                                from = parsed_from
-                                fd:write("250 OK\r\n")
-                                handled = true
-                            else
-                                fd:write("555 Invalid address\r\n")
-                                handled = true
-                            end
-                        else
-                            fd:write("555 Invalid MAIL command\r\n")
-                            handled = true
-                        end
-                    elseif msg_type == "RCPT" then
-                        if line:sub(6, 8) == "TO:" then
-                            local raw_to = line:sub(9)
-                            local parsed_to = self:extract_email(raw_to)
-                            if raw_to then
-                                if to == nil then to = {} end
-                                if #to >= 100 then
-                                    fd:write("555 Limit for number of recipients is 100\r\n")
-                                    handled = true
-                                else
-                                    to[#to + 1] = parsed_to
-                                    fd:write("250 OK\r\n")
-                                    handled = true
-                                end
-                            else
-                                fd:write("555 Invalid address\r\n")
-                                handled = true
-                            end
-                        else
-                            fd:write("555 Invalid RCPT command\r\n")
-                        end
-                    elseif msg_type == "DATA" then
-                        if from and to and #to > 0 and hello then
-                            fd:write("354 Send message content; end with <CR><LF>.<CR><LF>\r\n")
-                            message = coroutine.yield("\r\n.\r\n")
-                            if message ~= nil then
-                                self.counter = self.counter + 1
-                                local messageId = NODE_ID .. "-" .. self.counter
-                                for key, callback in pairs(self.on_mail_received_callbacks) do
-                                    local cb_ok, result = pcall(callback, {
-                                        from = from,
-                                        to = to,
-                                        body = message,
-                                        sender = {aio:get_ip(fd)},
-                                        id = messageId
-                                    })
-                                    if not cb_ok then
-                                        print("[smtp] mail handler " .. key .. " failed with " .. result)
-                                    end
-                                end
-                                from = nil
-                                message = nil
-                                to = {}
-                                fd:write("250 OK: queued as " .. messageId .. "\r\n")
-                                handled = true
-                            else
-                                fd:write("555 Message was missing\r\n")
-                                handled = true
-                            end
-                        else
-                            local errors = {"555-There were following errors:"}
-                            if not hello then errors[#errors+1] = "555- No hello has been sent" end
-                            if not from then errors[#errors+1] = "555- MAIL FROM has been never sent" end
-                            if not to then errors[#errors+1] = "555- RCPT TO has been never sent" end
-                            if to and #to == 0 then errors[#errors+1] = "555- There were zero recipients" end
-                            errors[#errors+1] = "555 Please, fill the missing information"
-                            fd:write(table.concat(errors, "\r\n") .. "\r\n")
-                            handled = true
-                        end
-                    elseif msg_type == "QUIT" then
-                        fd:write("221 Bye\r\n", true)
-                        handled = true
-                    end
-                    if not handled then
-                        fd:write("555 Unhandled message type\r\n")
-                    end
-                end
-            end)
+            self:handle_as_smtp(fd)
         end
     })
+end
+
+function smtp:handle_as_smtp(fd)
+    aio:buffered_cor(fd, function (_)
+        local ok = true
+        local hello = false
+        local from, message, to = nil, nil, {}
+        while ok do
+            local line = coroutine.yield("\r\n")
+            if line == nil then return end
+            local msg_type = line:sub(1, 4)
+            local handled = false
+            if msg_type == "HELO" then
+                hello = true
+                fd:write("250 HELO " .. line:sub(6) .. "\r\n")
+                handled = true
+            elseif msg_type == "EHLO" then
+                hello = true
+                fd:write("250-".. self.host .. " is my domain name. Hello " .. line:sub(6) .. "!\r\n250-8BITMIME\r\n250-ENHANCEDSTATUSCODES\r\n250 SIZE 1000000\r\n")
+                handled = true
+            elseif msg_type == "MAIL" then
+                if line:sub(6, 10) == "FROM:" then
+                    local raw_from = line:sub(11)
+                    local parsed_from = self:extract_email(raw_from)
+                    if from ~= nil then
+                        fd:write("555 MAIL FROM was already sent previously\r\n")
+                        handled = true
+                    elseif parsed_from then
+                        from = parsed_from
+                        fd:write("250 OK\r\n")
+                        handled = true
+                    else
+                        fd:write("555 Invalid address\r\n")
+                        handled = true
+                    end
+                else
+                    fd:write("555 Invalid MAIL command\r\n")
+                    handled = true
+                end
+            elseif msg_type == "RCPT" then
+                if line:sub(6, 8) == "TO:" then
+                    local raw_to = line:sub(9)
+                    local parsed_to = self:extract_email(raw_to)
+                    if raw_to then
+                        if to == nil then to = {} end
+                        if #to >= 100 then
+                            fd:write("555 Limit for number of recipients is 100\r\n")
+                            handled = true
+                        else
+                            to[#to + 1] = parsed_to
+                            fd:write("250 OK\r\n")
+                            handled = true
+                        end
+                    else
+                        fd:write("555 Invalid address\r\n")
+                        handled = true
+                    end
+                else
+                    fd:write("555 Invalid RCPT command\r\n")
+                end
+            elseif msg_type == "DATA" then
+                if from and to and #to > 0 and hello then
+                    fd:write("354 Send message content; end with <CR><LF>.<CR><LF>\r\n")
+                    message = coroutine.yield("\r\n.\r\n")
+                    if message ~= nil then
+                        self.counter = self.counter + 1
+                        local messageId = NODE_ID .. "-" .. self.counter
+                        for key, callback in pairs(self.on_mail_received_callbacks) do
+                            local cb_ok, result = pcall(callback, {
+                                from = from,
+                                to = to,
+                                body = message,
+                                sender = {aio:get_ip(fd)},
+                                id = messageId
+                            })
+                            if not cb_ok then
+                                print("[smtp] mail handler " .. key .. " failed with " .. result)
+                            end
+                        end
+                        from = nil
+                        message = nil
+                        to = {}
+                        fd:write("250 OK: queued as " .. messageId .. "\r\n")
+                        handled = true
+                    else
+                        fd:write("555 Message was missing\r\n")
+                        handled = true
+                    end
+                else
+                    local errors = {"555-There were following errors:"}
+                    if not hello then errors[#errors+1] = "555- No hello has been sent" end
+                    if not from then errors[#errors+1] = "555- MAIL FROM has been never sent" end
+                    if not to then errors[#errors+1] = "555- RCPT TO has been never sent" end
+                    if to and #to == 0 then errors[#errors+1] = "555- There were zero recipients" end
+                    errors[#errors+1] = "555 Please, fill the missing information"
+                    fd:write(table.concat(errors, "\r\n") .. "\r\n")
+                    handled = true
+                end
+            elseif msg_type == "QUIT" then
+                fd:write("221 Bye\r\n", true)
+                handled = true
+            end
+            if not handled then
+                fd:write("555 Unhandled message type\r\n")
+            end
+        end
+    end)
 end
 
 ---Extract information about sender
